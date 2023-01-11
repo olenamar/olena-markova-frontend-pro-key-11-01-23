104b45876895d424ee57f5ff15b77673ce56d65b
Olena <7890572@ukr.net>
2023-01-11 19:53:15 +0100

    Added a file

--- key.js
+++ key.js
@@ -3,8 +3,8 @@ const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 function generateKey(length, characters){
     let key = '';
     for (let i = 0; i < length; i++){
-        let generateNumber = Math.floor(Math.random() * (characters.length));
-        key += characters[generateNumber];
+        let generateSymbol = Math.floor(Math.random() * (characters.length));
+        key += characters[generateSymbol];
     }
     return key;
 }
